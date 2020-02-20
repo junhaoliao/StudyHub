47e985cddf2becc00f24ef92f181305e0f7e80e7
Junhao Liao <junhao@junhao.ca>
2020-02-20 05:28:05 -0500

    update App.js from the last commit (not committed successfully)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React from "react";
 
-import "./App.css";
+import './App.css';
+// import bootstrap style
+import 'bootstrap/dist/css/bootstrap.css';
 // Importing react-router-dom to use the React Router
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 // Importing pages
-import Home from "./react-components/Home";
-import Page404 from "./react-components/Page404";
-import { LoginBox } from "./react-components/LoginBox/login_box";
-import { SignupBox } from "./react-components/SignupBox/signup_box";
+import Home from './react-components/Home';
+import dash_board from './react-components/dash_board';
+import CSC309A from './react-components/CSC309A';
+import Page404 from './react-components/Page404';
 
 function App() {
-  return (
-    // <div>
-    //     <BrowserRouter>
-    //         <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */}
-    //             { /* Each Route below shows a different component depending on the exact path in the URL  */}
-    //             <Route path="/" exact component={Home}/>
+    return (
+        <div>
+            <BrowserRouter>
+                <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */}
+                    { /* Each Route below shows a different component depending on the exact path in the URL  */}
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/dash_board" exact component={dash_board}/>
+                    <Route path="/CSC309A" exact component={CSC309A}/>
 
-    //             <Route path="*" component={Page404}/>
-    //         </Switch>
-    //     </BrowserRouter>
-    // </div>
-    //<LoginBox></LoginBox>
-    <SignupBox></SignupBox>
-  );
+                    <Route path="*" component={Page404}/>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
