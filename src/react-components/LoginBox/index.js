df2b552dec26c11b733ff55386d43d03bd9aa414
kevinzhang <kevinhr.zhang@mail.utoronto.ca>
2020-02-27 11:53:09 -0500

    Change loginBox and SignupBox with semantic

---
+++ src/react-components/LoginBox/index.js
@@ -0,0 +1,120 @@
+import React from "react";
+import "./styles.css";
+import Home from "../dash_board/index";
+export class LoginBox extends React.Component {
+  // hard code for existing user log in information
+  username_list = ["Junhao", "Ashley", "Kruzer", "Kevin"];
+  password_list = ["Admin", "Regular", "Regular", "Regular"];
+
+  state = {
+    username: "",
+    password: "",
+    login: false
+  };
+
+  input_handler = event => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  submit_handler = event => {
+    console.log(this.state);
+    event.preventDefault();
+    let correct = false;
+    if (this.state.username == "") {
+      console.log("Username cannot be empty");
+    }
+    if (this.state.password == "") {
+      console.log("Password cannot be empty");
+    }
+    for (
+      let user_index = 0;
+      user_index < this.username_list.length;
+      user_index++
+    ) {
+      if (
+        this.username_list[user_index] == this.state.username &&
+        this.password_list[user_index] == this.state.password
+      ) {
+        correct = true;
+      }
+    }
+
+    if (correct) {
+      console.log(
+        "Welcome " + this.state.username + " " + this.state.password + " User"
+      );
+      this.setState(
+        {
+          login: true
+        },
+        () => {
+          console.log(this.state.login);
+        }
+      );
+    } else {
+      console.log("Incorrect username or password");
+      this.setState(
+        {
+          login: false
+        },
+        () => {
+          console.log(this.state.login);
+        }
+      );
+    }
+  };
+
+  render() {
+    if (!this.state.login) {
+      return (
+        /*The overall login box*/
+
+        <div className="ui placeholder segment">
+          <div className="field">
+            <label className="ui blue basic label">Username</label>
+            <div className="ui left icon input">
+              <input
+                className="username_input"
+                name="username"
+                type="text"
+                placeholder="Username"
+                value={this.state.username}
+                onChange={this.input_handler}
+              ></input>
+
+              <i className="blue user icon"></i>
+            </div>
+          </div>
+          <div className="field">
+            <label className="ui blue basic label">Password</label>
+            <div className="ui left icon input">
+              <input
+                className="password_input"
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={this.state.password}
+                onChange={this.input_handler}
+              ></input>
+              <i className="blue lock icon"></i>
+            </div>
+          </div>
+          <div className="field">
+            <button
+              className="ui primary button"
+              type="submit"
+              onClick={this.submit_handler}
+            >
+              Log In
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      return <Home></Home>;
+    }
+  }
+}
---
+++ src/react-components/SignupBox/index.js
@@ -0,0 +1,164 @@
+import React from "react";
+
+export class SignupBox extends React.Component {
+  state = {
+    username: "",
+    gender: "",
+    gpa: "",
+    education: "",
+    discipline: "",
+    password: "",
+    confirm_password: ""
+  };
+
+  input_handler = event => {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  submit_handler = event => {
+    let correct = true;
+    if (this.state.username == "") {
+      console.log("Username cannot be empty");
+      correct = false;
+    }
+    if (this.state.education == "") {
+      console.log("Level of Education cannot be empty");
+      correct = false;
+    }
+    if (this.state.discipline == "") {
+      console.log("Field of Study cannot be empty");
+      correct = false;
+    }
+    if (this.state.password == "" || this.state.confirm_password == "") {
+      console.log("Password cannot be empty");
+      correct = false;
+    }
+    if (this.state.password != this.state.confirm_password) {
+      console.log("Password does NOT match");
+      correct = false;
+    }
+    if (correct) {
+      console.log("Welcome to StudentHub! " + this.state.username);
+      console.log(this.state);
+    }
+  };
+  render() {
+    return (
+      <div className="ui placeholder segment">
+        <div className="field">
+          <label className="ui blue basic label">Username</label>
+          <div className="ui input focus">
+            <input
+              className="username_input"
+              name="username"
+              type="text"
+              placeholder="Username"
+              value={this.state.username}
+              onChange={this.input_handler}
+            ></input>
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="ui blue basic label">Gender</label>
+          <div>
+            <select className="ui dropdown">
+              <option value=""></option>
+              <option value="1">Male</option>
+              <option value="0">Female</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="ui blue basic label">GPA</label>
+          <div className="ui input focus">
+            <input
+              className="gpa_input"
+              name="gpa"
+              type="text"
+              placeholder="GPA"
+              value={this.state.gpa}
+              onChange={this.input_handler}
+            ></input>
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="ui blue basic label">Level of Education</label>
+          <div>
+            <select
+              name="education"
+              value={this.state.education}
+              onChange={this.input_handler}
+            >
+              <option></option>
+              <option>1st Year</option>
+              <option>2nd Year</option>
+              <option>3rd Year</option>
+              <option>4th Year</option>
+              <option>Master</option>
+              <option>Ph.D.</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="ui blue basic label">Field of Study</label>
+          <div>
+            <select
+              name="discipline"
+              value={this.state.discipline}
+              onChange={this.input_handler}
+            >
+              <option></option>
+              <option>CS</option>
+              <option>ECE</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="ui blue basic label">Password</label>
+          <div className="ui input focus">
+            <input
+              className="password_input"
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.input_handler}
+            ></input>
+          </div>
+        </div>
+
+        <div className="field">
+          <label className="ui blue basic label">Confirm Password</label>
+          <div className="ui input focus">
+            <input
+              className="confirm_password_input"
+              name="confirm_password"
+              type="password"
+              placeholder="Confirm Password"
+              value={this.state.confirm_password}
+              onChange={this.input_handler}
+            ></input>
+          </div>
+        </div>
+
+        <div className="field">
+          <button
+            className="ui blue primary button"
+            type="submit"
+            onClick={this.submit_handler}
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
