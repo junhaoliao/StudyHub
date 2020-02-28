b4eb389b06cf12a9c1598b1abf9cba0ab64d6e8d
Junhao Liao <junhao@junhao.ca>
2020-02-28 16:18:15 -0500

    Add billboard, modified App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import dash_board from './react-components/dash_board';
 import CSC309A from './react-components/CSC309A';
 import CSC309A_Resources from './react-components/CSC309A/resources';
 import Page404 from './react-components/Page404';
+import hub_Billboard from "./react-components/bill_board/bill_board";
 
 //import 'semantic-ui-css/semantic.min.css'
 
@@ -24,6 +25,7 @@ class App extends React.Component {
                         <Route path="/dash_board" exact component={dash_board}/>
                         <Route path="/CSC309A" exact component={CSC309A}/>
                         <Route path="/CSC309A/resources" exact component={CSC309A_Resources}/>
+                        <Route path="/bill_board" exact component={hub_Billboard}/>
 
                         <Route path="*" component={Page404}/>
                     </Switch>
