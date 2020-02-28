8dee67df863430b80800b3f5186b75a67dcc6f76
zhan4341 <49137066+zhan4341@users.noreply.github.com>
2020-02-28 10:04:17 -0500

    Add files via upload
    Change the style of Signin Box.

--- src/react-components/SignupBox/index.js
+++ src/react-components/SignupBox/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./styles.css";
 export class SignupBox extends React.Component {
   state = {
     username: "",
@@ -47,116 +47,133 @@ export class SignupBox extends React.Component {
   };
   render() {
     return (
-      <div className="ui placeholder segment">
-        <div className="field">
-          <label className="ui blue basic label">Username</label>
-          <div className="ui input focus">
-            <input
-              className="username_input"
-              name="username"
-              type="text"
-              placeholder="Username"
-              value={this.state.username}
-              onChange={this.input_handler}
-            ></input>
-          </div>
+      <div>
+        <div class="ui attached message">
+          <h2 class="ui blue center aligned icon header">
+            <i class="circular users icon"></i>
+            Welcome to StudentHub
+          </h2>
         </div>
-
-        <div className="field">
-          <label className="ui blue basic label">Gender</label>
-          <div>
-            <select className="ui dropdown">
-              <option value=""></option>
-              <option value="1">Male</option>
-              <option value="0">Female</option>
-            </select>
+        <form className="ui form attached fluid segment">
+          <div className="field">
+            <label className="ui blue basic label">Username</label>
+            <div className="ui blue input focus">
+              <input
+                className="username_input"
+                name="username"
+                type="text"
+                placeholder="Username"
+                value={this.state.username}
+                onChange={this.input_handler}
+              ></input>
+            </div>
           </div>
-        </div>
 
-        <div className="field">
-          <label className="ui blue basic label">GPA</label>
-          <div className="ui input focus">
-            <input
-              className="gpa_input"
-              name="gpa"
-              type="text"
-              placeholder="GPA"
-              value={this.state.gpa}
-              onChange={this.input_handler}
-            ></input>
+          <div className="two fields">
+            <div className="field">
+              <label className="ui blue basic label">Gender</label>
+              <div>
+                <select className="ui dropdown">
+                  <option value=""></option>
+                  <option value="1">Male</option>
+                  <option value="0">Female</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="field">
+              <label className="ui blue basic label">GPA</label>
+              <div className="ui input focus">
+                <input
+                  className="gpa_input"
+                  name="gpa"
+                  type="text"
+                  placeholder="GPA"
+                  value={this.state.gpa}
+                  onChange={this.input_handler}
+                ></input>
+              </div>
+            </div>
           </div>
-        </div>
 
-        <div className="field">
-          <label className="ui blue basic label">Level of Education</label>
-          <div>
-            <select
-              name="education"
-              value={this.state.education}
-              onChange={this.input_handler}
+          <div className="two fields">
+            <div className="field">
+              <label className="ui blue basic label">Level of Education</label>
+              <div>
+                <select
+                  name="education"
+                  value={this.state.education}
+                  onChange={this.input_handler}
+                >
+                  <option></option>
+                  <option>1st Year</option>
+                  <option>2nd Year</option>
+                  <option>3rd Year</option>
+                  <option>4th Year</option>
+                  <option>Master</option>
+                  <option>Ph.D.</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="field">
+              <label className="ui blue basic label">Field of Study</label>
+              <div>
+                <select
+                  name="discipline"
+                  value={this.state.discipline}
+                  onChange={this.input_handler}
+                >
+                  <option></option>
+                  <option>CS</option>
+                  <option>ECE</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="ui blue basic label">Password</label>
+            <div className="ui input focus">
+              <input
+                className="password_input"
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={this.state.password}
+                onChange={this.input_handler}
+              ></input>
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="ui blue basic label">Confirm Password</label>
+            <div className="ui input focus">
+              <input
+                className="confirm_password_input"
+                name="confirm_password"
+                type="password"
+                placeholder="Confirm Password"
+                value={this.state.confirm_password}
+                onChange={this.input_handler}
+              ></input>
+            </div>
+          </div>
+
+          <div className="field">
+            <button
+              className="ui blue primary button"
+              type="submit"
+              onClick={this.submit_handler}
             >
-              <option></option>
-              <option>1st Year</option>
-              <option>2nd Year</option>
-              <option>3rd Year</option>
-              <option>4th Year</option>
-              <option>Master</option>
-              <option>Ph.D.</option>
-            </select>
+              Sign Up
+            </button>
           </div>
-        </div>
+        </form>
 
-        <div className="field">
-          <label className="ui blue basic label">Field of Study</label>
-          <div>
-            <select
-              name="discipline"
-              value={this.state.discipline}
-              onChange={this.input_handler}
-            >
-              <option></option>
-              <option>CS</option>
-              <option>ECE</option>
-            </select>
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="ui blue basic label">Password</label>
-          <div className="ui input focus">
-            <input
-              className="password_input"
-              name="password"
-              type="password"
-              placeholder="Password"
-              value={this.state.password}
-              onChange={this.input_handler}
-            ></input>
-          </div>
-        </div>
-
-        <div className="field">
-          <label className="ui blue basic label">Confirm Password</label>
-          <div className="ui input focus">
-            <input
-              className="confirm_password_input"
-              name="confirm_password"
-              type="password"
-              placeholder="Confirm Password"
-              value={this.state.confirm_password}
-              onChange={this.input_handler}
-            ></input>
-          </div>
-        </div>
-
-        <div className="field">
-          <button
-            className="ui blue primary button"
-            type="submit"
-            onClick={this.submit_handler}
-          >
-            Sign Up
-          </button>
+        <div className="ui bottom attached warning message">
+          <i className="icon help"></i>
+          Already signed up? <a href="#">Login here</a> instead.
         </div>
       </div>
     );
