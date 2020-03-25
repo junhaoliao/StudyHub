e800ed591dfbc6917e00ba4a179b3f3395a4da58
kevinzhang <kevinhr.zhang@mail.utoronto.ca>
2020-03-25 00:03:42 -0400

    Adding a few API routines.

--- models/RegularUser.js
+++ models/RegularUser.js
@@ -2,6 +2,7 @@
 "use strict";
 
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const CourseSchema = new mongoose.Schema({
   title: String,
@@ -19,43 +20,43 @@ const RegularUserSchema = new mongoose.Schema({
   courseTaking: [CourseSchema]
 });
 
-// RegularUserSchema.pre("save", function(next) {
-//   const user = this;
-//   if (user.isModified("password")) {
-//     // generate salt and hash the password
-//     bcrypt.genSalt(10, (err, salt) => {
-//       bcrypt.hash(user.password, salt, (err, hash) => {
-//         user.password = hash;
-//         next();
-//       });
-//     });
-//   } else {
-//     next();
-//   }
-// });
+RegularUserSchema.pre("save", function(next) {
+  const user = this;
+  if (user.isModified("password")) {
+    // generate salt and hash the password
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
-// RegularUserSchema.statics.findByUsernamePassword = function(
-//   username,
-//   password
-// ) {
-//   const User = this;
+RegularUserSchema.statics.findByUsernamePassword = function(
+  username,
+  password
+) {
+  const User = this;
 
-//   return User.findOne({ username: username }).then(user => {
-//     if (!user) {
-//       return Promise.reject(); // a rejected promise
-//     }
-//     // if the user exists, make sure their password is correct
-//     return new Promise((resolve, reject) => {
-//       bcrypt.compare(password, user.password, (err, result) => {
-//         if (result) {
-//           resolve(user);
-//         } else {
-//           reject();
-//         }
-//       });
-//     });
-//   });
-// };
+  return User.findOne({ username: username }).then(user => {
+    if (!user) {
+      return Promise.reject(); // a rejected promise
+    }
+    // if the user exists, make sure their password is correct
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
 
 const RegularUser = mongoose.model("RegularUser", RegularUserSchema);
 
--- server.js
+++ server.js
@@ -15,6 +15,34 @@ const { RegularUser } = require("./models/RegularUser");
 
 console.log("Welcome to server.js");
 
+// Create a session cookie
+// app.use(
+//   session({
+//     secret: "oursecret",
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       expires: 60000,
+//       httpOnly: true
+//     }
+//   })
+// );
+app.get("/RegularUser/username/password", (req, res) => {
+  console.log("Access User");
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log("username: " + username);
+  console.log("password: " + password);
+
+  RegularUser.findByUsernamePassword(username, password)
+    .then(user => {
+      res.send(user);
+    })
+    .catch(error => {
+      res.status(400).send();
+    });
+});
+
 app.post("/RegularUser", (req, res) => {
   console.log("post a new regular user");
   const new_RegularUser = new RegularUser({
@@ -35,6 +63,82 @@ app.post("/RegularUser", (req, res) => {
       res.status(400).send(error);
     }
   );
+});
+
+app.get("/CourseTeaching", (req, res) => {
+  console.log("Check all courses taking");
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log("username: " + username);
+  console.log("password: " + password);
+
+  RegularUser.findByUsernamePassword(username, password)
+    .then(user => {
+      res.send(user.courseTeaching);
+    })
+    .catch(error => {
+      res.status(400).send();
+    });
+});
+
+app.post("/CourseTeaching", (req, res) => {
+  console.log("Add a course to teach");
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log("username: " + username);
+  console.log("password: " + password);
+
+  RegularUser.findByUsernamePassword(username, password)
+    .then(user => {
+      const new_course = {
+        title: req.body.title,
+        admin: req.body.username
+      };
+      user.courseTeaching.push(new_course);
+      user.save();
+      res.send(user);
+    })
+    .catch(error => {
+      res.status(400).send();
+    });
+});
+
+app.get("/CourseTaking", (req, res) => {
+  console.log("Check all courses taking");
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log("username: " + username);
+  console.log("password: " + password);
+
+  RegularUser.findByUsernamePassword(username, password)
+    .then(user => {
+      res.send(user.courseTaking);
+    })
+    .catch(error => {
+      res.status(400).send();
+    });
+});
+
+app.post("/CourseTaking", (req, res) => {
+  console.log("Add a course to take");
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log("username: " + username);
+  console.log("password: " + password);
+
+  RegularUser.findByUsernamePassword(username, password)
+    .then(user => {
+      const new_course = {
+        title: req.body.title,
+        admin: req.body.username
+      };
+      user.courseTaking.push(new_course);
+      user.save();
+      res.send(user);
+    })
+    .catch(error => {
+      res.status(400).send();
+    });
 });
 
 const port = process.env.PORT || 5000;
