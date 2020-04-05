9f962962389a033528a245ef8671ca609110c145
Junhao Liao <junhao@junhao.ca>
2020-04-04 21:15:06 -0400

    fixed Home user description covering icon
    Former-commit-id: 23b90c765de9d719452a5270014b3ba922e1fcae

--- client/src/react-components/Home/index.js
+++ client/src/react-components/Home/index.js
@@ -4,12 +4,8 @@ import "./styles.css";
 
 import {SignupBox} from "../SignupBox";
 import {LoginBox} from "../LoginBox";
-import {Button} from "semantic-ui-react";
-import {Item} from "semantic-ui-react";
-import {Grid, Image, Segment, Menu} from 'semantic-ui-react'
-import Divider from "semantic-ui-react/dist/commonjs/elements/Divider";
+import {Button, Grid, Item, Menu, Segment} from "semantic-ui-react";
 import List from "semantic-ui-react/dist/commonjs/elements/List";
-import Input from "semantic-ui-react/dist/commonjs/elements/Input";
 import Icon from "semantic-ui-react/dist/commonjs/elements/Icon";
 
 
@@ -103,10 +99,12 @@ class Home extends React.Component {
                                             <Icon name='book' size='small'/> You can add or join a course here.
                                         </List.Item>
                                         <List.Item>
-                                            <Icon name='clipboard' size='small'/> All the courses you are teaching or taking will be listed.
+                                            <Icon name='clipboard' size='small'/> All the courses you are teaching or
+                                            taking will be listed.
                                         </List.Item>
                                         <List.Item>
-                                            <Icon name='hand point up' size='small'/> Click "Enter" on the course to see announcement and upload files.
+                                            <Icon name='hand point up' size='small'/> Click "Enter" on the course to see
+                                            announcement and upload files.
                                         </List.Item>
                                     </div>
                                 </List>
@@ -118,13 +116,16 @@ class Home extends React.Component {
                                     <List>
                                         <div className={"fontsize"}>
                                             <List.Item>
-                                                <Icon name='comment alternate' size='small'/> You can chat will other users here.
+                                                <Icon name='comment alternate' size='small'/> You can chat will other
+                                                users here.
                                             </List.Item>
                                             <List.Item>
-                                                <Icon name='file alternate' size='small'/> Up to 50 most recent messages will be kept here.
+                                                <Icon name='file alternate' size='small'/> Up to 50 most recent messages
+                                                will be kept here.
                                             </List.Item>
                                             <List.Item>
-                                                <Icon name='question' size='small'/> Post the question to ask for help from other users when you are stuck!
+                                                <Icon name='question' size='small'/> Post the question to ask for help
+                                                from other users when you are stuck!
                                             </List.Item>
                                         </div>
                                     </List>
@@ -136,13 +137,16 @@ class Home extends React.Component {
                                         <List>
                                             <div className={"fontsize"}>
                                                 <List.Item>
-                                                    <Icon name='chart bar outline' size='small'/> You can see all the courses from the website by ranking.
+                                                    <Icon name='chart bar outline' size='small'/> You can see all the
+                                                    courses from the website by ranking.
                                                 </List.Item>
                                                 <List.Item>
-                                                    <Icon name='thumbs up' size='small'/> Hit the like bottom on a course to boost its rank!
+                                                    <Icon name='thumbs up' size='small'/> Hit the like bottom on a
+                                                    course to boost its rank!
                                                 </List.Item>
                                                 <List.Item>
-                                                    <Icon name='sort amount down' size='small'/> Let other users know which course is on top hits on the website!
+                                                    <Icon name='sort amount down' size='small'/> Let other users know
+                                                    which course is on top hits on the website!
                                                 </List.Item>
                                             </div>
                                         </List>
@@ -153,13 +157,16 @@ class Home extends React.Component {
                                         <List>
                                             <div className={"fontsize"}>
                                                 <List.Item>
-                                                    <Icon name='mouse pointer' size='small'/> Click to know more about our website!
+                                                    <Icon name='mouse pointer' size='small'/> Click to know more about
+                                                    our website!
                                                 </List.Item>
                                                 <List.Item>
-                                                    <Icon name='add user' size='small'/> Sign up within a minute to become a user!
+                                                    <Icon name='add user' size='small'/> Sign up within a minute to
+                                                    become a user!
                                                 </List.Item>
                                                 <List.Item>
-                                                    <Icon name='check' size='small'/> Study will never be so easy like this!
+                                                    <Icon name='check' size='small'/> Study will never be so easy like
+                                                    this!
                                                 </List.Item>
                                             </div>
 
@@ -179,94 +186,80 @@ class Home extends React.Component {
 
                         <Grid.Column>
                             <Segment>
-                                <Grid divided={"vertically"}>
-                                    <Grid.Row>
-                                        <Grid.Column width={2}>
-                                            <div className={"user_type_icon_container"}><Icon name='user'
-                                                                                              size='huge'/></div>
-                                        </Grid.Column>
-                                        <Grid.Column width={13}>
-                                            <Item.Group>
-                                                <Item><Item.Content>
-                                                    <Item.Header>Become a User!</Item.Header>
-                                                    <Item.Meta>
-                                                        <List bulleted>
-                                                            <List.Item>
-                                                                Sign up as a user can join any class to learn different
-                                                                courses.
-                                                            </List.Item>
-                                                            <List.Item>
-                                                                You can download or upload any file you want and discuss
-                                                                with other users in the chat room.
-                                                            </List.Item>
-                                                            <List.Item>
-                                                                Feel free to like a course if you like the Instructor.
-                                                            </List.Item>
-                                                            <List.Item>
-                                                                Favourite a file so that you can access it later in your
-                                                                own profile page.
-                                                            </List.Item>
-                                                            <List.Item>
-                                                                Learning will become easier since joining the StudyHub!
-                                                            </List.Item>
-                                                        </List>
-                                                    </Item.Meta>
-                                                </Item.Content></Item>
-                                                {/*<Item.Image size='medium' src='https://react.semantic-ui.com/images/wireframe/image.png'/>*/}
-
-
-                                            </Item.Group>
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                </Grid>
-
-
+                                <div className={"user_type_icon_container"}><Icon name='user'
+                                                                                  size='huge'/></div>
+                                <div className={"user_description_container"}>
+                                    <Item.Group>
+                                        <Item>
+                                            <Item.Content>
+                                                <Item.Header>Become a User!</Item.Header>
+                                                <Item.Meta>
+                                                    <List bulleted>
+                                                        <List.Item>
+                                                            Sign up as a user can join any class to learn different
+                                                            courses.
+                                                        </List.Item>
+                                                        <List.Item>
+                                                            You can download or upload any file you want and discuss
+                                                            with other users in the chat room.
+                                                        </List.Item>
+                                                        <List.Item>
+                                                            Feel free to like a course if you like the Instructor.
+                                                        </List.Item>
+                                                        <List.Item>
+                                                            Favourite a file so that you can access it later in your
+                                                            own profile page.
+                                                        </List.Item>
+                                                        <List.Item>
+                                                            Learning will become easier since joining the StudyHub!
+                                                        </List.Item>
+                                                    </List>
+                                                </Item.Meta>
+                                            </Item.Content>
+                                        </Item>
+                                    </Item.Group>
+                                </div>
                             </Segment>
                         </Grid.Column>
 
                         <Grid.Column>
                             <Segment>
-                                <Grid divided={"vertically"}>
-                                    <Grid.Row>
-                                        <Grid.Column width={2}>
-                                            <div className={"user_type_icon_container"}><Icon name='user secret'
-                                                                                              size='huge'/></div>
-                                        </Grid.Column>
-                                        <Grid.Column width={13}>
-                                            <Item.Group>
-                                                <Item><Item.Content>
-                                                    <Item.Header>Become an Instructor!</Item.Header>
-                                                    <Item.Meta>
-                                                        <List bulleted verticalAlign={"middle"}>
-                                                            <List.Item>
-                                                                Any regular user signed in the StudyHub can create a new
-                                                                course by
-                                                                himself/herself.
-                                                            </List.Item>
-                                                            <List.Item>
-                                                                Create your own course page and you are all set!
-                                                            </List.Item>
-                                                            <List.Item>
-                                                                Start uploading some useful course related files and
-                                                                posting some
-                                                                announcements.
-                                                            </List.Item>
-                                                            <List.Item>
-                                                                Try to post some questions and let other users learn
-                                                                through discussion.
-                                                            </List.Item>
-                                                            <List.Item>
-                                                                It's not that hard to become a teacher!
-                                                            </List.Item>
-                                                        </List>
-                                                    </Item.Meta>
-                                                </Item.Content></Item>
+                                <div className={"user_type_icon_container"}><Icon name='user secret'
+                                                                                  size='huge'/></div>
+                                <div className={"user_description_container"}>
+                                    <Item.Group>
+                                        <Item>
+                                            <Item.Content>
+                                                <Item.Header>Become an Instructor!</Item.Header>
+                                                <Item.Meta>
+                                                    <List bulleted verticalAlign={"middle"}>
+                                                        <List.Item>
+                                                            Any regular user signed in the StudyHub can create a new
+                                                            course by
+                                                            himself/herself.
+                                                        </List.Item>
+                                                        <List.Item>
+                                                            Create your own course page and you are all set!
+                                                        </List.Item>
+                                                        <List.Item>
+                                                            Start uploading some useful course related files and
+                                                            posting some
+                                                            announcements.
+                                                        </List.Item>
+                                                        <List.Item>
+                                                            Try to post some questions and let other users learn
+                                                            through discussion.
+                                                        </List.Item>
+                                                        <List.Item>
+                                                            It's not that hard to become a teacher!
+                                                        </List.Item>
+                                                    </List>
+                                                </Item.Meta>
+                                            </Item.Content>
+                                        </Item>
 
-                                            </Item.Group>
-                                        </Grid.Column>
-                                    </Grid.Row>
-                                </Grid>
-
+                                    </Item.Group>
+                                </div>
                             </Segment>
                         </Grid.Column>
                     </Grid>
